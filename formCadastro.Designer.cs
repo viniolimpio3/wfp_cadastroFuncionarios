a6b33cd5dd122d1e9bd1d234482be5cc870b4765
viniolimpio3 <viniolimpio3@gmail.com>
2021-10-16 10:07:10 -0300

    Inicializando Interface

---
+++ formCadastro.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace cadastroFuncionarios {
+    partial class frmFuncionarios {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.Label label3;
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.MaskedTextBox();
+            this.txtData = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(15, 228);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Location = new System.Drawing.Point(96, 228);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(75, 23);
+            this.btnCarregar.TabIndex = 1;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(30, 54);
+            this.txtCodigo.Mask = "999999";
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Código";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(136, 54);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(379, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(521, 54);
+            this.txtData.Mask = "00/00/0000";
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(100, 20);
+            this.txtData.TabIndex = 5;
+            this.txtData.ValidatingType = typeof(System.DateTime);
+            this.txtData.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtData_MaskInputRejected);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(518, 35);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(104, 13);
+            label3.TabIndex = 7;
+            label3.Text = "Data de Nascimento";
+            // 
+            // frmFuncionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 263);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.btnSalvar);
+            this.Name = "frmFuncionarios";
+            this.Text = "Cadastro de Funcionários";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.MaskedTextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox txtData;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ formCadastro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastroFuncionarios {
+    public partial class frmFuncionarios : Form {
+        public frmFuncionarios() {
+            InitializeComponent();
+        }
+
+        private void txtData_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) {
+            MessageBox.Show("Data Inválida!");
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
